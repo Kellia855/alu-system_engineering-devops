9b876e8c71c3a5391588a31eca713a77a4048d3b
Kellia855 <k.kamikazi@alustudent.com>
2025-08-04 13:45:47 +0200

    ssh config

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,21 @@
-file { '/home/kellia/.ssh/config':
-  ensure  => file,
-  owner   => 'kellia',
-  group   => 'kellia',
-  mode    => '0600',
-}
+# Puppet manifest to configure global SSH client settings in /etc/ssh/ssh_config
+# It sets the identity file to ~/.ssh/school and disables password authentication
 
-file_line { 'Declare identity file':
-  path  => '/home/kellia/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^\\s*IdentityFile\\s+',
-}
-
-file_line { 'Turn off passwd auth':
-  path  => '/home/kellia/.ssh/config',
-  line  => '    PasswordAuthentication no',
-  match => '^\\s*PasswordAuthentication\\s+',
-}
-
-file_line { 'Add Host wildcard':
-  path  => '/home/kellia/.ssh/config',
+file_line { 'Add Host *':
+  path  => '/etc/ssh/ssh_config',
   line  => 'Host *',
-  match => '^Host \\*',
+  match => '^Host \*',
+}
+
+file_line { 'Set IdentityFile':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^\s*IdentityFile\s+',
+}
+
+file_line { 'Disable password authentication':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^\s*PasswordAuthentication\s+',
 }
 
